0842c3cd2a325bea709942e75500e8d1a4f42b64
Valeria <lerik13@gmail.com>
2024-02-08 16:31:33 -0700

    Pagination for Projects

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
 import Todos from "./components/Todos"
+import Projects from "./components/Projects"
 
 function App() {
 	return (
-		<Todos />
+		<>
+			{/*<Todos />*/}
+			<Projects />
+		</>
 	)
 }
 
---
+++ client/src/components/Projects.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { useProjects } from "../services/queries"
+
+const Projects = () => {
+	const [page, setPage] = useState(1);
+
+	const {data, isPending, error, isError, isPlaceholderData, isFetching } = useProjects(page);
+console.log(data)
+	return (
+		<div>
+			<h2>Projects:</h2>
+			
+			{isPending ? (
+				<div>Loading...</div>
+			) : isError ? (
+				<div>Error: {error.message}</div>
+			) : (
+				<div>
+					{data.map((project) => (
+						<p key={project.id}>{project.name}</p>
+					))}
+				</div>
+			)}
+			<span>Current page: {page}</span>
+			{" "}
+			<button onClick={() => setPage((old) => Math.max(old - 1, 1))}>
+				Previous Page
+			</button>
+			{" "}
+			<button
+				onClick={() => {
+					if (!isPlaceholderData) { setPage((old) => old + 1) }
+				}}
+				disabled={isPlaceholderData}
+			>
+				Next Page
+			</button>
+
+			{isFetching ? <span>Loading...</span> : null}
+
+		</div>
+	)
+}
+
+export default Projects
